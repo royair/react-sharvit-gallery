c210c74ddcc5e45d84f52a7be867b052e8e61567
roy <roy.yair@gmail.com>
2019-04-04 21:54:31 +0300

    Updates

---
+++ precache-manifest.2437610890704ae15ec3e1cc40f266ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df4e937ffa615f202c41",
+    "url": "/react-sharvit-gallery/static/js/runtime~main.35a7828a.js"
+  },
+  {
+    "revision": "7dce73dba57446365ad5",
+    "url": "/react-sharvit-gallery/static/js/main.9d69d57a.chunk.js"
+  },
+  {
+    "revision": "dd0e6d8a32a3c565eb28",
+    "url": "/react-sharvit-gallery/static/js/2.0cb68539.chunk.js"
+  },
+  {
+    "revision": "7dce73dba57446365ad5",
+    "url": "/react-sharvit-gallery/static/css/main.e7df2926.chunk.css"
+  },
+  {
+    "revision": "4ab60137707a6e1329667a8861ad683d",
+    "url": "/react-sharvit-gallery/index.html"
+  }
+];
